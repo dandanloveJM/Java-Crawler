5066c0af54feb4d29ae08e55b03ef4345bbffb1e
dandanloveJM <gigi834921748@qq.com>
2020-04-19 22:44:54 +0100

    加入数据库
    引入H2数据库，并且进一步重构代码

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,5 +1,6 @@
 package com.github.hcsp;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -12,43 +13,105 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 
+import java.io.File;
 import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        //待处理的链接池
-        List<String> linkpool = new ArrayList<>();
-        //已经处理的链接池
-        Set<String> processedLinks = new HashSet<>();
-        linkpool.add("https://sina.cn");
+    private static final String USER_NAME = "root";
+    private static final String USER_PASSWORD = "dandan";
+
+    private static List<String> loadUrlsFromDatabase(Connection connection, String sql) throws SQLException {
+        List<String> results = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                results.add(resultSet.getString(1));
+            }
+        }
+        return results;
+    }
+
+    @SuppressFBWarnings("DMI_CONSTANT_DB_PASSWORD")
+    public static void main(String[] args) throws IOException, SQLException {
         while (true) {
+            //从数据库加载即将处理的链接
+            Connection connection = getConnection();
+            List<String> linkpool = loadUrlsFromDatabase(connection, "select link from LINKS_TO_BE_PROCESSED");
             if (linkpool.isEmpty()) {
                 break;
             }
             //arraylist从尾部删除更有效率, remove会返回删掉的元素
+            //先在数据库中拿出来一个Link，再在数据库里删掉这个Link
             String link = linkpool.remove(linkpool.size() - 1);
-            if (processedLinks.contains(link)) {
+            try (PreparedStatement statement = connection.prepareStatement("delete from LINKS_TO_BE_PROCESSED where link = ?")) {
+                statement.setString(1, link);
+                statement.executeUpdate();
+            }
+
+            //询问数据库当前链接是不是已经被处理过了
+            if (isLinkProcessed(connection, link)) {
                 continue;
             }
             if (IsInterestedLink(link)) {
                 //这是我们感兴趣的，我们只处理新浪站内的链接
                 Document doc = httpGetAndParseHtml(link);
                 //找到有用的a链接且放进链接池
-                doc.select("a").stream().map(aTag->aTag.attr("href")).forEach(linkpool::add);
-
+                parseUrlsFromPageAndStoreIntoDatabase(connection, doc);
                 //假如这是一个新闻的详情页面，就存入数据库，否则什么都不做
                 storeIntoDatabaseIfItIsNews(doc);
-                processedLinks.add(link);
-            } else {
-                //我们不感兴趣的链接，不处理它
-                continue;
+                //把已经处理过的链接放进LINKS_ALREADY_PROCESSED
+                insertLinkIntoDatabase(connection, link, "insert into LINKS_ALREADY_PROCESSED (link) values (?)");
+
+
+            }
+
+
+        }
+    }
+
+    private static void parseUrlsFromPageAndStoreIntoDatabase(Connection connection, Document doc) throws SQLException {
+        for (Element aTag : doc.select("a")) {
+            String href = aTag.attr("href");
+            if (IsInterestedLink(href)) {
+                insertLinkIntoDatabase(connection, href, "insert into LINKS_TO_BE_PROCESSED (link) values (?)");
             }
         }
     }
+
+    @SuppressFBWarnings("DMI_CONSTANT_DB_PASSWORD")
+    private static Connection getConnection() throws SQLException {
+        File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
+        String jdbcUrl = "jdbc:h2:file:" + new File(projectDir, "news").getAbsolutePath();
+        return DriverManager.getConnection(jdbcUrl, USER_NAME, USER_PASSWORD);
+    }
+
+    private static boolean isLinkProcessed(Connection connection, String link) throws SQLException {
+        ResultSet resultSet = null;
+        try (PreparedStatement statement = connection.prepareStatement("select link from LINKS_ALREADY_PROCESSED where link = ?")) {
+            statement.setString(1, link);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                return true;
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+        return false;
+    }
+
+    private static void insertLinkIntoDatabase(Connection connection, String link, String sql) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, link);
+            statement.executeUpdate();
+        }
+    }
+
 
     private static void storeIntoDatabaseIfItIsNews(Document doc) {
         Elements articleTags = doc.select("article");
